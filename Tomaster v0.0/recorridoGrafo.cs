d76e3eebe6e2beee49388d51ad4884391f0954ee
Francisco Adaros Tapia <adarosfrancisco817@gmail.com>
2022-12-01 23:27:00 -0300

    Clase recorridoGrafo

---
+++ Tomaster v0.0/recorridoGrafo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomaster_v0._0
+{
+    internal class recorridoGrafo
+    {
+        void DFS(grafo &g, int s)
+        {
+            nodoVisitado[s] = true;
+            procesarNodo(s);
+            list<int>::iterator it;
+
+            for (it = g.lados[s].begin(); it != g.lados[s].end(); ++it)
+            {
+                if (!nodoVisitado[*it])
+                {
+                    nodoPadre[*it] = s;
+                    DFS(g, *it);
+                }
+                else if (!nodoProcesado[*it])
+                {
+                    procesarLado(s, *it);
+                }
+            }
+            nodoProcesado[s] = true;
+        }
+    }
+}
